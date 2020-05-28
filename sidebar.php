7fc91d6feab7fa49c9b610ab1f26a7f1b1aaeb69
pegiatcrypto <40342291+pegiatcrypto@users.noreply.github.com>
2020-05-28 13:51:58 +0700

    Delete sidebar.php

--- sidebar.php
+++
@@ -1,9 +0,0 @@
-<aside class="sidebar" role="complementary">
-
-	<?php get_template_part( 'searchform' ); ?>
-
-	<div class="widget">
-		<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-post-1' ) ) ?>
-	</div>
-
-</aside>
